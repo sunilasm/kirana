1bd5a480293bc337f46b1facc5121d6c212ff60e
root <root@ip-172-31-0-225.ap-south-1.compute.internal>
2018-12-07 08:24:55 +0000

    Marketplace extension updates.

--- app/etc/env.php
+++ app/etc/env.php
@@ -29,19 +29,19 @@ return [
         'save' => 'files'
     ],
     'cache_types' => [
-        'config' => 0,
-        'layout' => 0,
-        'block_html' => 0,
-        'collections' => 0,
-        'reflection' => 0,
-        'db_ddl' => 0,
-        'eav' => 0,
-        'customer_notification' => 0,
-        'config_integration' => 0,
-        'config_integration_api' => 0,
-        'full_page' => 0,
-        'config_webservice' => 0,
-        'translate' => 0
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
     ],
     'install' => [
         'date' => 'Tue, 06 Nov 2018 13:45:04 +0000'
