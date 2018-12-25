a462df5893338046a4fac4005475248548889fd9
sunilasm <sunil.n@altencalsoftlabs.com>
2018-12-25 11:54:48 +0530

    Product search extension setup.

---
+++ app/code/Asm/Search/view/frontend/web/js/custom_search.js
@@ -0,0 +1,7 @@
+require([
+"jquery"
+], function($){
+    $(document).ready(function() {
+         //alert("Hi, I am from custom_search.js");
+    });
+});
--- app/etc/config.php
+++ app/etc/config.php
@@ -5,6 +5,7 @@ return [
         'Magento_Directory' => 1,
         'Magento_Eav' => 1,
         'Asm_Geolocation' => 1,
+        'Asm_Search' => 1,
         'Magento_Theme' => 1,
         'Magento_Cms' => 1,
         'Magento_Customer' => 1,
