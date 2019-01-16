85b92ffd56f3f5eb525c5f2c8ce204dfab2065b7
pradeeps112 <pradeep.ramrao@altencalsoftlabs.com>
2019-01-16 12:52:48 +0530

    Search cart clear.

---
+++ app/code/Asm/AdvanceSearch/Controller/Rewrite/Advanced/Clearcart.php
@@ -0,0 +1,23 @@
+<?php
+use Magento\Checkout\Model\Cart;
+use Magento\Checkout\Model\Session as CheckoutSession;
+use Magento\Framework\App\Action\Action;
+class Clearcart extends Action
+{
+	protected  $_modelCart;
+	protected $ checkoutSession;
+	public function __construct(CheckoutSession $checkoutSession,Cart $modelCart)
+        {
+        	$this->checkoutSession = $checkoutSession;
+        	$this->_modelCart = $modelCart;
+        }
+		public function execute()
+        {
+			$cart = $this->_modelCart;
+			$quoteItems = $this->checkoutSession->getQuote()->getItemsCollection();
+			foreach($quoteItems as $item)
+			{
+				$cart->removeItem($item->getId())->save(); 
+			}
+		}
+}
