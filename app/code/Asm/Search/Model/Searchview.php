d320b50ad57f3c1008a1ee014d97ce8e26efc1e1
sunilasm <sunil.n@altencalsoftlabs.com>
2019-02-01 18:35:16 +0530

    QA ver 1.44

--- app/code/Asm/Search/Model/Searchview.php
+++ app/code/Asm/Search/Model/Searchview.php
@@ -68,66 +68,65 @@ class Searchview implements SearchInterface
        // return $data;
    // }
 
-    public function deletesku() {
+    // public function deletesku() {
 
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        $request = $objectManager->get('\Magento\Framework\Webapi\Rest\Request');
-        $request->getBodyParams();
-        $post = $request->getBodyParams();
-        //print_r($post);exit;
-        $quoteId = $post['quote_id'];
-        $sku = $post['sku'];
+    //     $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+    //     $request = $objectManager->get('\Magento\Framework\Webapi\Rest\Request');
+    //     $request->getBodyParams();
+    //     $post = $request->getBodyParams();
+    //     //print_r($post);exit;
+    //     $quoteId = $post['quote_id'];
+    //     $sku = $post['sku'];
 
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        $quoteModel = $objectManager->create('Magento\Quote\Model\Quote');
-        $quoteItems = $quoteModel->load($quoteId)->getAllVisibleItems();
-        $quoteItemArray = array();
-        $i = 1;
-        foreach($quoteItems as $item):
-            // print_r($item);
-            $quoteItemArray[$i] = $item->getSku();
-            $quoteItemIndexArray[$i] = $item->getItemid();
-            $i++;
-        endforeach;
-        //print_r($quoteItemIndexArray);exit;
-        $data = '';
-        $message = 'You have no items in your shopping cart.';
-        if(count($quoteItemArray)){
-            $ArrayIndex = array_search($sku, $quoteItemArray);
-            //print_r($ArrayIndex);exit;
-            if($ArrayIndex){
+    //     $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+    //     $quoteModel = $objectManager->create('Magento\Quote\Model\Quote');
+    //     $quoteItems = $quoteModel->load($quoteId)->getAllVisibleItems();
+    //     $quoteItemArray = array();
+    //     $i = 1;
+    //     foreach($quoteItems as $item):
+    //         // print_r($item);
+    //         $quoteItemArray[$i] = $item->getSku();
+    //         $quoteItemIndexArray[$i] = $item->getItemid();
+    //         $i++;
+    //     endforeach;
+    //     //print_r($quoteItemIndexArray);exit;
+    //     $data = '';
+    //     $message = 'You have no items in your shopping cart.';
+    //     if(count($quoteItemArray)){
+    //         $ArrayIndex = array_search($sku, $quoteItemArray);
+    //         //print_r($ArrayIndex);exit;
+    //         if($ArrayIndex){
 
-                // Get base Url
-                $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-                $storeManager = $objectManager->get('\Magento\Store\Model\StoreManagerInterface');
-                //$baseUrl = $storeManager->getStore()->getBaseUrl();
-		$baseUrl = "http://13.233.41.0/";
-		//print_r($baseUrl); exit;
-                $userData = array("username" => "sunil.n", "password" => "Admin@123");
-                $ch = curl_init("$baseUrl".''."rest/V1/integration/admin/token");
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($userData));
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Content-Lenght: " . strlen(json_encode($userData))));
+    //             // Get base Url
+    //             $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+    //             $storeManager = $objectManager->get('\Magento\Store\Model\StoreManagerInterface');
+    //             //$baseUrl = $storeManager->getStore()->getBaseUrl();
+	// 	$baseUrl = "http://13.233.41.0/";
+	// 	//print_r($baseUrl); exit;
+    //             $userData = array("username" => "sunil.n", "password" => "Admin@123");
+    //             $ch = curl_init("$baseUrl".''."rest/V1/integration/admin/token");
+    //             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    //             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($userData));
+    //             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //             curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Content-Lenght: " . strlen(json_encode($userData))));
 
-                $token = curl_exec($ch);
-		//echo $token; exit;
-                $ch = curl_init("$baseUrl".''."rest/V1/carts/".$quoteId."/items/".$quoteItemIndexArray[$ArrayIndex]);
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Authorization: Bearer " . json_decode($token)));
+    //             $token = curl_exec($ch);
+	// 	//echo $token; exit;
+    //             $ch = curl_init("$baseUrl".''."rest/V1/carts/".$quoteId."/items/".$quoteItemIndexArray[$ArrayIndex]);
+    //             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+    //             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //             curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Authorization: Bearer " . json_decode($token)));
 
-                $result = curl_exec($ch);
-		//print_r($result); exit;
-                $result = json_decode($result, 1);
-                $message = 'Sku is successfully removed from cart.';
-            }
-        }
-        $data = array('status'=>'Sucess','message' => $message);
-        //print_r($data);
-        exit;
-        //return $data;
-    }
+    //             $result = curl_exec($ch);
+	// 	//print_r($result); exit;
+    //             $result = json_decode($result, 1);
+    //             $message = 'Sku is successfully removed from cart.';
+    //         }
+    //     }
+    //     $data = array('status'=>'Sucess','message' => $message);
+    //     print_r($data);exit;
+    //     return $data;
+    // }
 
     // public function checkcart() {
 
