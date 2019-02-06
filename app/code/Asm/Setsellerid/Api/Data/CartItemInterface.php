f7d2ef8271d38d42ce164b2419a2bfe83ba25df0
sunilasm <sunil.n@altencalsoftlabs.com>
2019-02-06 16:29:01 +0530

    QA compile issue.

--- app/code/Asm/Setsellerid/Api/Data/CartItemInterface.php
+++ app/code/Asm/Setsellerid/Api/Data/CartItemInterface.php
@@ -24,11 +24,6 @@ interface CartItemInterface
      * @return int|null Item seller_id. Otherwise, null.
      */
     public function getSellerId();
-   /* public function getSellerId()
-    {
-        return $this->getData('seller_id');
-    }*/
-
     /**
      * Sets the item seller_id.
      *
@@ -36,19 +31,6 @@ interface CartItemInterface
      * @return $this
      */
     public function setSellerId($seller_id);
-   /* public function setSellerId($seller_id)
-    {
-        return $this->setData('seller_id', $seller_id);
-    }*/
-     public function getImageUrl()
-    {
-        return $this->_get('image_url');
-    }
-
-     public function setImageUrl($imageUrl)
-    {
-        $this->setData('image_url', $imageUrl);
-        return $this;
-    }
-  
+    public function getImageUrl();
+    public function setImageUrl($imageUrl);
 }
