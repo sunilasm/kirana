6a3fa84195c7c3e2df0a3c47cfe16eaea4106bff
pradeeps112 <pradeep.ramrao@altencalsoftlabs.com>
2019-02-07 10:12:12 +0530

    Add calculatedistance  file

---
+++ calculatedistance.php
@@ -0,0 +1,25 @@
+<?php
+function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+  if (($lat1 == $lat2) && ($lon1 == $lon2)) {
+    return 0;
+  }
+  else {
+    $theta = $lon1 - $lon2;
+    $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+    $dist = acos($dist);
+    $dist = rad2deg($dist);
+    $miles = $dist * 60 * 1.1515;
+    $unit = strtoupper($unit);
+
+    if ($unit == "K") {
+      return ($miles * 1.609344);
+    } else if ($unit == "N") {
+      return ($miles * 0.8684);
+    } else {
+      return $miles;
+    }
+  }
+}
+echo distance(19.0149418, 73.0135926, 19.0973309, 73.0013923, "K") . " Kilometers<br>";
+
+?>
