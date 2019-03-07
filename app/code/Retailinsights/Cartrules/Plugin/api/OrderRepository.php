19c8f836b782ea8826a6c3282e9f9803688da650
Bhargaw <bhargaw.rss@theretailinsights.com>
2019-03-07 06:08:15 -0700

    order item extension attributes fix

--- app/code/Retailinsights/Cartrules/Plugin/api/OrderRepository.php
+++ app/code/Retailinsights/Cartrules/Plugin/api/OrderRepository.php
@@ -1,59 +1,48 @@
 <?php
-
 namespace Retailinsights\Cartrules\Plugin\api;
-
 use Magento\Catalog\Api\ProductRepositoryInterfaceFactory as ProductRepository;
 use Magento\Sales\Api\Data\OrderExtensionFactory;
+use Magento\Sales\Api\Data\OrderItemExtensionFactory;
+
 use Magento\Sales\Api\Data\OrderInterface;
 use Magento\Sales\Api\Data\OrderSearchResultInterface;
 use Magento\Sales\Api\OrderRepositoryInterface;
 use Magento\Catalog\Helper\ImageFactory as ProductImageHelper;
-
-
 class OrderRepository
 {
-
     const FIELD_NAME = 'unitm';
-
     /**
-
     *@var \Magento\Catalog\Helper\ImageFactory
-
     */
-
     protected $productImageHelper;
-
     /**
-
     * @var ProductRepository
-
     */
-
     protected $productRepository;
-
     /**
      * Order Extension Attributes Factory
      *
      * @var OrderExtensionFactory
      */
     protected $extensionFactory;
-
     /**
      * OrderRepositoryPlugin constructor
      *
      * @param OrderExtensionFactory $extensionFactory
      */
+    protected $itemextensionFactory;
     public function __construct(
         ProductImageHelper $productImageHelper,
         OrderExtensionFactory $extensionFactory,
+        OrderItemExtensionFactory $itemextensionFactory,
         \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
     )
     {
+        $this->itemextensionFactory = $itemextensionFactory;
         $this->extensionFactory = $extensionFactory;
          $this->_productRepository = $productRepository;
          $this->productImageHelper = $productImageHelper;
     }
-
     public function afterGetList(OrderRepositoryInterface $subject, OrderSearchResultInterface $searchResult)
     {
    
@@ -62,13 +51,13 @@ class OrderRepository
                 $addAtt = array();
                 $info = array(); 
                 $addImage = array();
-                $infoImage = array();   
+                $infoImage = array(); 
+                $orderItems = $order->getItems();
             
-            foreach($order->getItems() as $items){
+            foreach($orderItems as $items){
              //$items->getProductId()
-
             $product = $this->_productRepository->getById($items->getProductId());
-
+            
               $uom = $product->getUnitm();
                $weight = round($product->getWeight(), 0);
                 $optionId = $product->getUnitm();
@@ -78,21 +67,19 @@ class OrderRepository
                 }
              $sku = $items->getSku();
              $imageurl =$this->productImageHelper->create()->init($product, 'product_thumbnail_image')->setImageFile($product->getThumbnail())->getUrl();
-            $addAtt[$sku] = $weight." ".$optionText;
-            $addImage[$sku] = $imageurl;
-
              $addAtt[$sku] = $weight." ".$optionText;
+             $addImage[$sku] = $imageurl;
+            $extensionAttributes = $items->getExtensionAttributes();
+            $extensionAttributes = $extensionAttributes ? $extensionAttributes : $this->itemextensionFactory->create();
+                        $extensionAttributes->setUnitm($optionText);
+                        $extensionAttributes->setImageUrl($imageurl);
+                                    $items->setExtensionAttributes($extensionAttributes);
 
+            
+             
             }
             $info[] = $addAtt; 
             $infoImage[] = $addImage;
-            $data = json_encode($info);
-            $dataImage = json_encode($infoImage);
-            $extensionAttributes = $items->getExtensionAttributes();
-            $extensionAttributes = $extensionAttributes ? $extensionAttributes : $this->extensionFactory->create();
-                        $extensionAttributes->setUnitm($data);
-                        $extensionAttributes->setImageUrl($dataImage);
-            $order->setExtensionAttributes($extensionAttributes);
             
         }
         return $searchResult;
