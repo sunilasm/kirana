f07dbcf4fbf294bb1dd5281c228fefb04163d46b
Bhargaw <bhargaw.rss@theretailinsights.com>
2019-06-14 06:40:18 -0700

    Updated sku specific

--- promotionCron.php
+++ promotionCron.php
@@ -44,29 +44,13 @@
 	$date = new DateTime();
 	$timeStamp = $date->getTimestamp();
 
-	$sellerprod_query 	= "SELECT * FROM `mglof_marketplace_product`";
-	$sellerprod_result 	= $connection->fetchall($sellerprod_query);
 			
 	$customrules_query  = "SELECT * FROM `mgretailinsights_promostoremapp` WHERE rule_type = 1 AND status = 1";  // only catalog rules
 	$customrules_result 	= $connection->fetchall($customrules_query);
 	
-	/**	
-	 * Get Admin Token
-	 * --------------- 
-	*/
-	$admin_token_url = "rest/V1/integration/admin/token";
-	$ch = curl_init('http://13.233.85.241:82/'.$admin_token_url);
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($admin_credentials));
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-		"Content-Type: application/json", 
-		"Content-Lenght: " . strlen(json_encode($admin_credentials))
-	));
-	$admin_token = curl_exec($ch);
-	//print_r($admin_token);die;
 
-	if ((count($sellerprod_result) <= 0)  || (count($customrules_result) <= 0)) {
+
+	if ( count($customrules_result) <= 0) {
 		# stop if no products or rules
 		exit();
 	}
@@ -74,53 +58,91 @@
 	$attribute_result = $connection->fetchall($attribute_query);
 	$attributeId = $attribute_result[0]['attribute_id'];
 	
-	foreach($customrules_result as $rule){
+	foreach($customrules_result as $rule) {
 	  $storeId = $rule['store_id'];
-		$storeproducts_query = "SELECT mkprod.*,catprod.value as mrp FROM `mglof_marketplace_product`as mkprod join `mgcatalog_product_entity_decimal` catprod on catprod.entity_id=mkprod.product_id where mkprod.seller_id = ".$storeId." and catprod.attribute_id = ".$attributeId ;
-		
-		$storeproducts_result = $connection->fetchall($storeproducts_query);
-        foreach($storeproducts_result as $product){ 
-			$mrp = $product['mrp'];		  
-			$sellerType = $rule['seller_type'];
-		if($sellerType== 1){  
-		   if((!empty($product['pickup_from_store'])) || ($product['pickup_from_store']!= NULL) || ($product['pickup_from_store']!=0)){ 
-			  $old_price = $product['pickup_from_store'];	
-			  if($rule['simple_action']=='by_fixed'){
-					$discount =  $rule['discount_amount'];
-					$new_price = $mrp - $discount;
-			   }else{
-				    $discount = ($mrp * $rule['discount_amount'])/100 ;
-				    $new_price = $mrp - $discount;
-			   }
-			   $new_price = roundUp($new_price,2);
-			   if(($product['pickup_from_store_old']==0) || ($product['pickup_from_store_old']==NULL)){ 
-			       $update = "UPDATE `mglof_marketplace_product` SET pickup_from_store_old = ".$old_price." , pickup_from_store = ".$new_price." WHERE entity_id =".$product['entity_id']." AND seller_id = ".$storeId;
-			       $connection->query($update);
-			   }
-		   }
+	  $skus = array();
+      $conditionsarr=array();
+	  $con_arr = json_decode($rule['conditions_serialized'] , true); 
+		if(!empty($con_arr['conditions'])) {
+			$conditionsarr = $con_arr['conditions'];
+			foreach($conditionsarr as $ck => $con){  
+				if($con['attribute']=='sku'){
+					$skus[] = $con['value'];
+				}
+				if(!empty($con['conditions'])){
+					foreach($con['conditions'] as $c_inn => $c_inn_val){
+						if($c_inn_val['attribute']=='sku'){
+							$skus[] = $c_inn_val['value'];
+						}
+					}
+				}
+			}
 		}
-		if($sellerType== 0){  
+		if(!empty($skus)){
+			$skus = explode(',', str_replace(' ','',$skus[0])); 
+			//print_r($skus);
+			foreach($skus as $sku) {
+				$storeproducts_query = "SELECT mkprod.*,count(*) as cnt,catprod.value as mrp, product.sku as sku FROM `mglof_marketplace_product`as mkprod join `mgcatalog_product_entity_decimal` as catprod on catprod.entity_id=mkprod.product_id join mgcatalog_product_entity product on product.entity_id = catprod.entity_id where mkprod.seller_id = $storeId and catprod.attribute_id = $attributeId and product.sku = '$sku' " ;
+				$cnt = $connection->rawFetchRow($storeproducts_query,'cnt');	
+				//print_r($cnt);
+				if($cnt){
+					$storeproducts_result = $connection->fetchall($storeproducts_query);
 
-		   if((!empty($product['doorstep_price'])) || ($product['doorstep_price']!= NULL) || ($product['doorstep_price']!=0)){ 
-			  $old_price = $product['doorstep_price']; 	
+					foreach($storeproducts_result as $product){ 
+						//print_r($storeproducts_result); echo "<br>";
+						$mrp = $product['mrp'];		  
+						$sellerType = $rule['seller_type'];
+					if($sellerType== 1){  
+					   if((!empty($product['pickup_from_store'])) || ($product['pickup_from_store']!= NULL) || ($product['pickup_from_store']!=0)){ 
+						  $old_price = $product['pickup_from_store'];	
+						  if($rule['simple_action']=='by_fixed'){
+								$discount =  $rule['discount_amount'];
+								$new_price = $mrp - $discount;
+						   }else{
+								$discount = ($mrp * $rule['discount_amount'])/100 ;
+								$new_price = $mrp - $discount;
+						   }
+						   $new_price = roundUp($new_price,2);
+						   if(($product['pickup_from_store_old']==0) || ($product['pickup_from_store_old']==NULL)){ 
+							   $update = "UPDATE `mglof_marketplace_product` SET pickup_from_store_old = ".$old_price." , pickup_from_store = ".$new_price." WHERE entity_id =".$product['entity_id']." AND seller_id = ".$storeId;
+							   print_r($update);echo "<br>";
+							   //print_r("pick");
+							   $connection->query($update);
+						   }
+					   }
+					}
+					if($sellerType== 0){  
+			
+					   if((!empty($product['doorstep_price'])) || ($product['doorstep_price']!= NULL) || ($product['doorstep_price']!=0)){ 
+						  $old_price = $product['doorstep_price']; 	
+			
+						  if($rule['simple_action']=='by_fixed'){
+								$discount =  $rule['discount_amount'];
+								$new_price = $mrp - $discount;
+							 }else{
+								$discount = ($mrp * $rule['discount_amount'])/100 ;
+								$new_price = $mrp - $discount;
+						  }
+						  $new_price = roundUp($new_price,2);
+						  if(($product['doorstep_price_old']==0) || ($product['doorstep_price_old']==NULL)){ 
+							  $update = "UPDATE `mglof_marketplace_product` SET doorstep_price_old = ".$old_price." , doorstep_price = ".$new_price." WHERE entity_id =".$product['entity_id']." AND seller_id = ".$storeId ;
+							  //print_r($update);echo "<br>";
+							  //print("door");
+							 $connection->query($update);
+						  }
+					   }
+					}
+			
+					}
 
-			  if($rule['simple_action']=='by_fixed'){
-					$discount =  $rule['discount_amount'];
-					$new_price = $mrp - $discount;
-		   	  }else{
-					$discount = ($mrp * $rule['discount_amount'])/100 ;
-					$new_price = $mrp - $discount;
-			  }
-			  $new_price = roundUp($new_price,2);
-			  if(($product['doorstep_price_old']==0) || ($product['doorstep_price_old']==NULL)){ 
-			      $update = "UPDATE `mglof_marketplace_product` SET doorstep_price_old = ".$old_price." , doorstep_price = ".$new_price." WHERE entity_id =".$product['entity_id']." AND seller_id = ".$storeId ;
-				  $connection->query($update);
-			  }
-		   }
-		}
+				}
+				}
+				
 
-		}
-	  
+				
+			
+			}
+			
 	}
 
 	function roundUp ( $value, $precision ) { 
