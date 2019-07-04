40126bf064efbd387916430eaad08509d6739fe5
sunilasm <sunil.n@aletencalsoftlabs.com>
2019-07-04 11:13:00 +0000

    export kirana product controller change.

--- app/code/TEXT/Smsnotifications/Controller/Index/Ordersms.php
+++ app/code/TEXT/Smsnotifications/Controller/Index/Ordersms.php
@@ -36,7 +36,7 @@ class Ordersms extends \Magento\Framework\App\Action\Action
                 //date_default_timezone_set('Asia/Kolkata'); 
                 $time = time();
                 $to = date('Y-m-d H:i:s', $time);
-                $lastTime = $time - 3000000; // 60*60*24
+                $lastTime = $time - 300; // 60*60*24
                 $from = date('Y-m-d H:i:s', $lastTime);
                 //print_r("to:-".$to);
                 //print_r("from:-".$from); exit;
@@ -100,7 +100,18 @@ class Ordersms extends \Magento\Framework\App\Action\Action
 		   // print_r($admin_recipients);
                     array_push($admin_recipients, $telephone);
 		    //print_r($admin_recipients);
-                    $result = $objectManager->get('TEXT\Smsnotifications\Helper\Data')->sendSms($text,$admin_recipients);
+                   // $result = $objectManager->get('TEXT\Smsnotifications\Helper\Data')->sendSms($text,$admin_recipients);
+		    $settings = array();
+                    $settings['sms_gateway_url'] = "https://api.textlocal.in/";
+                    $settings['sms_auth_token'] = 'LcWtONwzQAo-7XakJm9eP2SBMM7DTPEriHKBgwUqiR';
+                    $settings['sms_sender_name'] = 'ASMESP';
+
+                    $errors = array();
+                    $apiuri = $settings['sms_gateway_url'];
+                    $apiurl = $apiuri."send?&apiKey=".urlencode($settings['sms_auth_token'])."&sender=".urlencode($settings['sms_sender_name'])."&numbers=".urlencode(implode(',', $admin_recipients))."&message=".urlencode($text);
+                    //print_r($apiurl);exit;
+                    $rows = file_get_contents($apiurl);
+                    $result = json_decode($rows, true);
                 }
                 $table .= "<tr style='border:1px solid #000'>";
                 $table .= "<td style='border-right:1px solid #000'>";
