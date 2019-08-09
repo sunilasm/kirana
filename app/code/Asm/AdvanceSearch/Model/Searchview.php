6d17861a48af83bbf6ca065fb3e8df7d52d9c0ea
sunilasm <sunil.n@altencalsoftlabs.com>
2019-08-09 16:54:22 +0530

    Html tag issue

--- app/code/Asm/AdvanceSearch/Model/Searchview.php
+++ app/code/Asm/AdvanceSearch/Model/Searchview.php
@@ -360,8 +360,8 @@ class Searchview implements SearchInterface
             $entColl['thumbnail'] = $product->getData('thumbnail'); 
             $entColl['volume'] = $product->getData('volume');   
             $entColl['price'] = $product->getData('price');   
-            $entColl['description'] = $product->getData('description');   
-            $entColl['short_description'] = $product->getData('short_description');   
+            $entColl['description'] = strip_tags($product->getData('description'));   
+            $entColl['short_description'] = strip_tags($product->getData('short_description'));   
             //$entColl['unitm'] = (round($product->getData('weight'),0)).' '.($product->getData('uom_label'));
             $entColl['unitm'] = ($product->getData('volume')).' '.($product->getData('uom_label'));
             if(!empty($chsnOrgId && $chsnOrgPrice)){
